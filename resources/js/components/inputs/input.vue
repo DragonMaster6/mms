3aa72927f8e717d52381080787ad456850935ff4
Ben Matson <bjdmatson@gmail.com>
2021-06-07 14:41:37 +0000

    Adding the input field component

---
+++ resources/js/components/inputs/input.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="['flex', inline ? 'flex-row' : 'flex-col']">
+      <label v-if="label != ''"> {{ label }} </label>
+      <input :type="type" 
+            class="border-2 border-gray-300 rounded-lg px-2 py-1"
+            :value="value" 
+            @input="$emit('input', $event.target.value)">
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        inline: {
+            type: Boolean,
+            default: false,
+        },
+        label: {
+            type: String,
+            default: "",
+        },
+        type: {
+            type: String,
+            default: "text",
+        },
+        value: {
+            default: '',
+        },
+    },
+    data() {
+        return {
+            content: this.value,
+        }
+    },
+    methods: {
+        updateInput() {
+            this.$emit('input', this.content);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
