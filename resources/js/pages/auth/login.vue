fa282a3c38b5d9eb5ca795177c83ff85362c6021
Ben Matson <bmatson@pop-os.localdomain>
2022-03-15 19:19:56 -0600

    Addting Ineriajs, Vue3, and Tailwindcss

---
+++ resources/js/pages/auth/login.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const name = ref('Ben Matson')
+
+//  export default {
+//    setup: () => {
+//      const name = ref('Ben Matson')
+//    }
+//  }
+</script>
+
+<template>
+  <h1> Hello {{ name }} </h1>
+</template>
