37f010794a61e63fc56651eed298e29e86fd8ee9
Ben Matson <bjdmatson@gmail.com>
2022-08-26 16:32:39 -0600

    Adding mutator and accessor to Transaction value

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    //////////////////////
+    // Accessor Methods //
+    //////////////////////
+    /**
+     * Converts the monetary value into dollars
+     */
+    protected function value(): Attribute {
+      return Attribute::make(
+        get: fn ($value) => $value/100,
+        set: fn($value) => $value * 100
+      );
+    }
 }
