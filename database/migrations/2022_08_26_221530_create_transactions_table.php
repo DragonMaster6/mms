0828a12d0b5576a3d5f907767bacbbbe5b071a17
Ben Matson <bjdmatson@gmail.com>
2022-08-26 16:25:24 -0600

    Adding base Transaction model and migration

---
+++ database/migrations/2022_08_26_221530_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->comment('What this transaction represents');
+            $table->unsignedInteger('value')->comment('Value is in cents');
+            $table->dateTime('date')->comment('When the transaction transpired on Bank');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
